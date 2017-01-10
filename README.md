692a96a99fd46a1d7109077ec05bc556b01b0337
robins <lb.robin1991@gmail.com>
2017-01-10 15:53:59 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # vibrate.js
 A lightweight，non-dependent vibrate library for javascript
+
+
+### use
+
+#### setPattern
+// set default vibrate pattern;
+// Vibrate 'SOS' in Morse.
+// vibe.setPattern([pattern]);
+vibe.setPattern([1000,300,1000,300,1000,2000,2000,300,2000,300,2000,2000,1000,300,1000,300,1000]);
+or
+vibe.setPattern(200);
+
+// run vibrate once; if no arg, it will get the default pattern;
+// vibe.once([pattern]);
+vibe.once(200);// vibrate for 200ms
+
+// stop the vibrate anytime;
+vibe.stop();
+
+// run vibrate loop with gap; if no arg, it will get the default pattern;
+// vibe.loop([pattern, interval]);
+vibe.loop([1000, 100, 300], 100);
+
+### create vibration patterns
+
+[vibe.js tool](https://naschq.github.io/vibe.js/) by [felipenmoura](https://github.com/felipenmoura)
---
+++ src/vibrate.js
@@ -0,0 +1,50 @@
+;(function(win, vibe) {
+
+  var vibrate = "vibrate" in navigator ? 'vibrate' : "mozVibrate" in navigator ? 'mozVibrate' : undefined;
+
+  if (!vibrate){
+    console.warn('your browser is not support Vibration!');
+  }
+  var vibrateInterval;
+
+  function setDefaultPatterns (pattern) {
+    vibe.pattern = pattern;
+  }
+
+  // Starts vibration at passed in level
+  function startVibrate(duration) {
+    if (!vibrate) { return false;}
+    duration = duration || vibe.pattern;
+    win.navigator[vibrate](duration);
+  }
+
+  // Stops vibration
+  function stopVibrate() {
+    if (!vibrate) { return false;}
+    // Clear interval and stop persistent vibrating
+    if (vibrateInterval) clearInterval(vibrateInterval);
+    win.navigator[vibrate](0);
+  }
+
+  // Start persistent vibration at given duration and interval
+  // Assumes a number value is given
+  function startPeristentVibrate(duration, interval) {
+    duration = duration || vibe.pattern;
+    interval = interval || 0;
+    // 保证振动模式被执行完，所以这里interval被设定为间隙，而不是重头播放的间隔。
+    if(duration instanceof Array){
+      interval += duration.reduce(function(a, b){ return a + b; }, 0);
+    } else if (typeof duration === 'number'){
+      interval += duration;
+    }
+    vibrateInterval = setInterval(function() {
+      startVibrate(duration);
+    }, interval);
+  }
+
+  vibe.setPattern = setDefaultPatterns;
+  vibe.once = startVibrate;
+  vibe.stop = stopVibrate;
+  vibe.loop = startPeristentVibrate;
+
+})(window, window['vibe'] || (window['vibe'] = {}))
